3714c8740a583f4a8606cd08f59f03d7bbe3b7dd
Deun Lee <54697735+deunlee@users.noreply.github.com>
2020-10-13 15:52:59 +0900

    Refactoring Point class

--- src/com/c0destudy/sokoban/misc/Point.java
+++ src/com/c0destudy/sokoban/misc/Point.java
@@ -7,14 +7,8 @@ public class Point implements Serializable
     private int x;
     private int y;
 
-    public Point(final int x, final int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Point(final Point point) {
-        this(point.getX(), point.getY());
-    }
+    public Point(final int x, final int y) { this.x = x; this.y = y;           }
+    public Point(final Point point)        { this(point.getX(), point.getY()); }
 
     public int getX() { return x; }
     public int getY() { return y; }
@@ -22,19 +16,19 @@ public class Point implements Serializable
     public void setY(final int y) { this.y = y; }
 
     public void add(final Point point) {
-        setX(getX() + point.getX());
-        setY(getY() + point.getY());
+        x += point.x;
+        y += point.y;
     }
 
-    public static Point add(final Point a, final Point b) {
-        return new Point(a.getX() + b.getX(), a.getY() + b.getY());
-    }
+    public static Point add(final Point a, final Point b) { return new Point(a.x + b.x, a.y + b.y); }
+    public static Point sub(final Point a, final Point b) { return new Point(a.x - b.x, a.y - b.y); }
+    public static Point reverse(final Point a)            { return new Point(-a.x, -a.y);                 }
 
     @Override
     public boolean equals(Object object) {
         if (object instanceof Point) {
             final Point point = (Point)object;
-            return getX() == point.getX() && getY() == point.getY();
+            return x == point.x && y == point.y;
         }
         return false;
     }
