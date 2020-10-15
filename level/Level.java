2cc3bc8bb11d6b00df8f6525201864ae15d0a4e6
Sersoc <mysky213@gmail.com>
2020-10-16 01:47:14 +0900

    add highscore

---
+++ level/Level.java
@@ -0,0 +1,364 @@
+package com.c0destudy.sokoban.level;
+
+import com.c0destudy.sokoban.misc.Point;
+import com.c0destudy.sokoban.misc.Resource;
+import com.c0destudy.sokoban.tile.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Level implements Serializable
+{
+    private static final long        serialVersionUID = 1L;
+    private final String             name;
+    private final int                width;
+    private final int                height;
+    private final int                difficult;
+    public  char                     high;
+    private final ArrayList<Wall>    walls    = new ArrayList<>();
+    private final ArrayList<Goal>    goals    = new ArrayList<>();
+    private final ArrayList<Baggage> baggages = new ArrayList<>();
+    private final ArrayList<Trigger> triggers = new ArrayList<>();
+    private final ArrayList<Player>  players  = new ArrayList<>();
+    private final ArrayList<Record>  records  = new ArrayList<>();
+    private boolean                  isRecordEnabled;
+    private long                     timeLastMove      = 0;
+    private int                      moveCount         = 0;
+    private int                      remainingBaggages = 0;
+    private int                      hp = 3;
+
+    public Level(final String name, final int width, final int height, final int difficult) {
+        this.name   = name;
+        this.width  = width;
+        this.height = height;
+        this.difficult = difficult;
+        setRecordEnabled(true);
+    }
+ 
+    // 레벨 정보
+    public String  getName()              { return name;      }
+    public int     getWidth()             { return width;     }
+    public int     getHeight()            { return height;    }
+    public int     getMoveCount()         { return moveCount; }
+    public int     getRemainingBaggages() { return remainingBaggages;      }
+    public int     getLeftHealth()        { return hp;                     }
+    public boolean isCompleted()          { return remainingBaggages == 0; }
+    public boolean isFailed()             { return hp == 0;                }
+    public boolean getRecordEnabled()     { return isRecordEnabled;        }
+    public void    setRecordEnabled(final boolean enabled) {
+        isRecordEnabled = enabled;
+        if (isRecordEnabled) {
+            timeLastMove = System.currentTimeMillis();
+        }
+    }
+
+    // 타일
+    public ArrayList<Wall>    getWalls()    { return walls;    }
+    public ArrayList<Goal>    getGoals()    { return goals;    }
+    public ArrayList<Baggage> getBaggages() { return baggages; }
+    public ArrayList<Player>  getPlayers()  { return players;  }
+    public ArrayList<Trigger> getTriggers() { return triggers; }    
+    public ArrayList<Movable> getMovables() {
+        final ArrayList<Movable> movables = new ArrayList<>();
+        movables.addAll(baggages);
+        movables.addAll(players);
+        return movables;
+    }
+    public Player getPlayer(final int index) {
+        try {
+            return players.get(index);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+    public Record getRecord(final int index) {
+        if (index >= 0 && index < records.size()) {
+            return records.get(index);
+        } else {
+            return null;
+        }
+    }
+    public void addWall   (final Wall wall)       { walls.add(wall);     }
+    public void addGoal   (final Goal goal)       { goals.add(goal);     }
+    public void addTrigger(final Trigger trigger) { triggers.add(trigger); }
+    public void addPlayer (final Player player)   { players.add(player); }
+    public void addBaggage(final Baggage baggage) { baggages.add(baggage); remainingBaggages++; }
+
+    public void reset() {
+        records.clear();
+        resetWithoutRecords();
+    }
+
+    public void resetWithoutRecords() {
+        for (final Movable movable : getMovables()) {
+            movable.setPosition(movable.getOriginalPosition());
+        }
+        timeLastMove      = System.currentTimeMillis();
+        hp                = 3;
+        moveCount         = 0;
+        remainingBaggages = 0;
+        for (final Baggage baggage : getBaggages()) {
+            if (!isGoalAt(baggage.getPosition())) {
+                remainingBaggages++;
+            }
+        }
+    }
+
+    /**
+     * 플레이어의 좌표 변화량을 입력받아 플레이어를 이동하고,
+     * 물건을 미는 경우 물건도 이동시킵니다.
+     *
+     * 이동하는 방향에 물건이 있으면 물건도 같이 이동시킵니다.
+     * 단, 물건이 연속으로 2개 있거나 벽이 있는 경우에는 이동할 수 없습니다.
+     *
+     * @param  playerIndex 이동할 플레이어 인덱스
+     * @param  direction   플레이어의 이동 방향 (좌표 변화량)
+     * @return             성공 여부
+     */
+    public boolean movePlayerAndBaggage(final int playerIndex, final Point direction) {
+        // 플레이어가 이동할 새로운 좌표 계산
+        final Player player       = getPlayer(playerIndex);
+        final Point  newPlayerPos = Point.add(player.getPosition(), direction);
+
+        // 이동할 위치에 벽이 있는 경우 => 이동 불가
+        if (isWallAt(newPlayerPos)) return false;
+
+        // 이동할 위치에 다른 플레이어가 있는 경우 => 이동 불가
+        if (isPlayerAt(newPlayerPos)) return false;
+        
+        Trigger trigger = getTriggerAt(newPlayerPos);
+        if (trigger != null) {
+        	hp--;
+        }
+
+        // 플레이어가 물건(baggage)을 미는 경우
+        final Baggage baggage = getBaggageAt(newPlayerPos);
+        if (baggage != null) {
+            if (!moveBaggage(baggage, direction)) return false;
+        }
+
+        // 기록 (undo와 replay를 위함)
+        if (getRecordEnabled()) {
+            final long   currentTime = System.currentTimeMillis();
+            final Record record      = new Record(
+                    currentTime - timeLastMove,
+                    playerIndex,
+                    player.getPosition(),
+                    direction,
+                    baggage != null);
+            records.add(record);
+            timeLastMove = currentTime;
+        }
+
+        // 플레이어 이동
+        player.moveDelta(direction);
+        moveCount++;
+        return true;
+    }
+
+    /**
+     * 물건을 이동시킵니다.
+     *
+     * 남은 물건의 개수는 자동으로 계산됩니다.
+     * 물건을 이동할 수 없거나 찾을 수 없는 경우에는 false를 반환합니다.
+     *
+     * @param  baggage   이동할 물건 객체
+     * @param  direction 물건의 이동 방향 (좌표 변화량)
+     * @return           성공 여부
+     */
+    private boolean moveBaggage(final Baggage baggage, final Point direction) {
+        // 물건이 이동될 새로운 좌표 계산
+        final Point position = Point.add(baggage.getPosition(), direction);
+
+        // 물건이 이동될 위치에 벽이 있는 경우 => 이동 불가
+        if (isWallAt(position)) return false;
+
+        // 물건이 이동될 위치에 물건이 있는 경우 (연속 2개) => 이동 불가
+        if (getBaggageAt(position) != null) return false;
+
+        // 물건이 이동될 위치에 다른 플레이어가 있는 경우 => 이동 불가
+        if (isPlayerAt(position)) return false;
+
+        // 이동 전에 물건이 목적지에 있는 경우
+        if (isGoalAt(baggage.getPosition())) remainingBaggages++;
+
+        // 물건을 이동시킬 수 있는 공간이 있으면 물건을 민다
+        baggage.moveDelta(direction);
+
+        // 이동 후에 물건이 목적지에 있는 경우
+        if (isGoalAt(baggage.getPosition())) remainingBaggages--;
+
+        return true;
+    }
+
+    /**
+     * 플레이어 이동을 취소합니다.
+     *
+     * 물건을 움직인 경우 물건도 이전 위치로 다시 이동시킵니다.
+     */
+    public void undoMove() {
+        if (!getRecordEnabled() || records.size() == 0) return;
+
+        // 마지막 움직임 기록 가져오기
+        final Record record    = records.remove(records.size() - 1);
+        final Player player    = getPlayer(record.getPlayerIndex());
+        final Point  position  = record.getPosition();
+        final Point  direction = record.getDirection();
+
+        // 역방향 이동
+        player.moveDelta(Point.reverse(direction));
+
+        // 물건을 민 경우 물건도 역방향으로 이동시킨다
+        if (record.isBaggageMoved()) {
+            final Baggage baggage = getBaggageAt(Point.add(position, direction, direction));
+            moveBaggage(baggage, Point.reverse(direction));
+        }
+
+        moveCount--;
+    }
+
+    /**
+     * 해당 좌표에 벽이 있는지 확인합니다.
+     *
+     * @param  position 좌표
+     * @return          벽의 존재 여부
+     */
+    private boolean isWallAt(final Point position) {
+        for (final Wall wall : walls) {
+            if (wall.getPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 해당 좌표에 목적지가 있는지 확인합니다.
+     *
+     * @param  position 좌표
+     * @return          목적지의 존재 여부
+     */
+    private boolean isGoalAt(final Point position) {
+        for (final Goal goal : goals) {
+            if (goal.getPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 해당 좌표에 플레이어가 있는지 확인합니다.
+     *
+     * @param  position 좌표
+     * @return          플레이어의 존재 여부
+     */
+    private boolean isPlayerAt(final Point position) {
+        for (final Player player : players) {
+            if (player.getPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean isTriggerAt(final Point position) {
+    	for(final Trigger trigger: triggers) {
+    		if(trigger.getPosition().equals(position)) {
+    			return true;
+    		}
+    	}
+		return false;
+    }
+
+    private Trigger getTriggerAt(final Point position) {
+    	for(final Trigger trigger: triggers) {
+    		if(trigger.getPosition().equals(position)) {
+    			return trigger;
+    		}
+    	}
+    	return null;
+    }
+
+
+    /**
+     * 해당 좌표에 있는 물건 객체를 가져옵니다.
+     *
+     * @param  position 좌표
+     * @return          물건 객체
+     */
+    private Baggage getBaggageAt(final Point position) {
+        for (final Baggage baggage : baggages) {
+            if (baggage.getPosition().equals(position)) {
+                return baggage;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 해당 좌표에 있는 플레이어 객체를 가져옵니다.
+     *
+     * @param  position 좌표
+     * @return          플레이어 객체
+     */
+    private Player getPlayerAt(final Point position) {
+        for (final Player player : players) {
+            if (player.getPosition().equals(position)) {
+                return player;
+            }
+        }
+        return null;
+    }
+    
+    public char getGrade() {//점수채점
+    	switch((getMoveCount() - difficult) / 10) {
+    	case 0:
+    		return 'A';
+    	case 1: 
+    		return 'B';
+    	case 2:
+    		return 'C';
+    	case 3:
+    		return 'D';
+    	case 4:
+    		return 'E';
+    	default:
+    		return 'F';
+    	}
+    }
+    
+    public char getHigh() {
+    	return high;
+    }
+    
+    public void recordHigh(char high) {
+    	System.out.println(name);
+    	try {
+    		File recordFile = new File("src/resources/levelscores/ high.txt");
+    		FileWriter fw = new FileWriter(recordFile);
+    		fw.write(high);
+    		fw.close();
+    	}catch(IOException e) {
+    		System.out.println(e);
+    	}
+    }
+    
+    public char callHigh() {
+    	
+    	try {
+    		File recordFile = new File("src/resources/levelscores/ high.txt");
+    		FileReader fr = new FileReader(recordFile);
+    		high = (char) fr.read();
+    		fr.close();
+    	}catch(IOException e) {
+    		System.out.println(e);
+    	}
+    	return high;
+    }
+}
---
+++ level/LevelManager.java
@@ -0,0 +1,161 @@
+package com.c0destudy.sokoban.level;
+
+import com.c0destudy.sokoban.misc.Point;
+import com.c0destudy.sokoban.misc.Resource;
+import com.c0destudy.sokoban.tile.Baggage;
+import com.c0destudy.sokoban.tile.Goal;
+import com.c0destudy.sokoban.tile.Player;
+import com.c0destudy.sokoban.tile.Wall;
+import com.c0destudy.sokoban.tile.Trigger;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LevelManager
+{
+    // txt 파일에서 사용하는 레벨 기호 모음
+    private static final char LEVEL_SYMBOL_WALL            = '#';
+    private static final char LEVEL_SYMBOL_BAGGAGE         = '$';
+    private static final char LEVEL_SYMBOL_BAGGAGE_AT_GOAL = '*';
+    private static final char LEVEL_SYMBOL_GOAL            = '.';
+    private static final char LEVEL_SYMBOL_PLAYER          = '@';
+    private static final char LEVEL_SYMBOL_PLAYER_AT_GOAL  = '+';
+    private static final char LEVEL_SYMBOL_TRIGGER         = '!';
+
+    /**
+     * 레벨 인스턴스를 생성합니다.
+     *
+     * @param  levelName 레벨 이름
+     * @return Level     레벨 인스턴스
+     */
+    public static Level getNewLevel(final String levelName) {
+        final Path    path    = Paths.get(String.format(Resource.PATH_LEVEL, levelName));
+        final Charset charset = StandardCharsets.UTF_8;
+        try {
+            final List<String> lines = Files.readAllLines(path, charset);
+            return getNewLevelFromStringList(levelName, lines);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 문자열 리스트로부터 레벨 인스턴스를 생성합니다.
+     *
+     * 레벨의 가로/세로 크기는 자동으로 계산됩니다.
+     *
+     * @param  levelName 레벨 이름
+     * @param  levelData 레벨 데이터가 행별로 구분된 리스트
+     * @return Level     레벨 인스턴스
+     */
+    private static Level getNewLevelFromStringList(final String levelName, final List<String> levelData) {
+        // 레벨의 가로, 세로 크기 계산
+        final int width = levelData
+                .stream()
+                .map(String::length)
+                .reduce(0, Math::max);
+        final int height = levelData.size();
+        Map<String, String> metaData = new HashMap<String, String>(); 
+        
+        // 레벨 메타데이터 불러오기
+        for (int i = 0; !levelData.get(i).equals("map"); ++i) {
+            final String[] scoreData = levelData.get(i).split(":");
+            metaData.put(scoreData[0], scoreData[1]);
+        }
+        // 레밸 인스턴스 생성
+        final Level level = new Level(levelName, width, height, Integer.parseInt(metaData.get("difficulty"), 10));
+
+        // 각종 블럭 추가
+        for (int y = 0; y < levelData.size(); y++) {
+            final String line = levelData.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                final Point point = new Point(x, y);
+                switch (line.charAt(x)) {
+                    case LEVEL_SYMBOL_WALL:
+                        level.addWall(new Wall(point));
+                        break;
+                    case LEVEL_SYMBOL_BAGGAGE:
+                        level.addBaggage(new Baggage(point));
+                        break;
+                    case LEVEL_SYMBOL_BAGGAGE_AT_GOAL:
+                        level.addBaggage(new Baggage(point));
+                        level.addGoal(new Goal(new Point(point)));
+                        break;
+                    case LEVEL_SYMBOL_GOAL:
+                        level.addGoal(new Goal(point));
+                        break;
+                    case LEVEL_SYMBOL_PLAYER:
+                        level.addPlayer(new Player(point));
+                        break;
+                    case LEVEL_SYMBOL_PLAYER_AT_GOAL:
+                        level.addPlayer(new Player(point));
+                        level.addGoal(new Goal(new Point(point)));
+                        break;
+                    case LEVEL_SYMBOL_TRIGGER:
+                    	level.addTrigger(new Trigger(point));
+                    	break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return level;
+    }
+
+    /**
+     * 일시 정지된 레벨 데이터가 존재하는지 확인합니다.
+     *
+     * @return boolean 존재 여부
+     */
+    public static boolean isPausedLevelExisting() {
+        return (new File(Resource.PATH_LEVEL_PAUSE)).exists();
+    }
+
+    /**
+     * 레벨 인스턴스를 직렬화하여 파일로 저장합니다.
+     *
+     * @param  level    레벨 인스턴스
+     * @param  filePath 파일 경로
+     * @return boolean  성공 여부
+     */
+    public static boolean saveLevelToFile(final Level level, final String filePath) {
+        try {
+            final FileOutputStream     fos = new FileOutputStream(filePath);
+            final BufferedOutputStream bos = new BufferedOutputStream(fos);
+            final ObjectOutputStream   out = new ObjectOutputStream(bos);
+            out.writeObject(level);
+            out.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 레벨 인스턴스를 파일에서 불러와 역직렬화합니다.
+     *
+     * @param  filePath 파일 경로
+     * @return Level    레벨 인스턴스
+     */
+    public static Level readLevelFromFile(final String filePath) {
+        try {
+            final FileInputStream     fis   = new FileInputStream(filePath);
+            final BufferedInputStream bis   = new BufferedInputStream(fis);
+            final ObjectInputStream   in    = new ObjectInputStream(bis);
+            final Level               level = (Level) in.readObject();
+            in.close();
+            return level;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
